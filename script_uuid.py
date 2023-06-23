9f8e09ac709a42cda1356436a39024c9e2424dba
elenautkina <selenamondeu@gmail.com>
2023-06-23 15:50:13 +0300

    add loops functions

--- script_uuid.py
+++ script_uuid.py
@@ -32,9 +32,37 @@ def third_function(identificator):
     return e
 
 
+# 4 вариант - в строке нельзя заменить символы по их индексу, поэтому собираем
+# еще одну строку посимвольно в цикле:
+# первые 8 символов берем из строки autotest, а оставшиеся из uuid
+def fourth_function(identificator):
+    string = 'autotest'
+    result = ''
+
+    for i in range(len(identificator)):
+        if i < 8:
+            result = result + string[i]
+        else:
+            result = result + identificator[i]
+    return result
+
+
+# 5 вариант - заменяем в листе uuid первые символы на все символы строки autotest
+# получаем готовый лист и преобразуем его в строку с помощью join
+def fifth_function(identificator):
+    string = 'autotest'
+    id_list = list(identificator)
+
+    for i in range(8):
+        id_list[i] = string[i]
+    return ''.join(id_list)
+
+
 id = str(uuid.uuid4())
 print(id)
 
 print(first_function(id))
 print(second_function(id))
 print(third_function(id))
+print(fourth_function(id))
+print(fifth_function(id))
